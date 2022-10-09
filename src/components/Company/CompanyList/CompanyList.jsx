037ead816cdc2218ef683736799315194f2749ad
Darymee <danieledaniele3005@gmail.com>
2022-10-09 18:54:21 +0200

    start

---
+++ src/components/Company/CompanyList/CompanyList.jsx
@@ -0,0 +1,28 @@
+import { CompanyUl } from './CompanyList.styled';
+
+
+
+export const CompanyList = ({ company }) => {
+  return (
+    <CompanyUl>
+      <li>
+        <h3></h3>
+        <div>
+          <img src="" alt=""></img>
+          <ul>
+            <li>
+              <p></p>
+            </li>
+            <li>
+              <p></p>
+            </li>
+            <li>
+              <p></p>
+            </li>
+          </ul>
+        </div>
+        <div></div>
+      </li>
+    </CompanyUl>
+  );
+};
---
+++ src/components/Company/CompanyList/CompanyList.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const CompanyUl = styled.ul`
+  display: flex;
+  gap: 15px;
+`;
